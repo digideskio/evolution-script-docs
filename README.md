bf21d6bbe9a7f9cb7c412fd6f31dbf67c6fcbb2d
Liam (Aqua) <liamdraws4@gmail.com>
2015-04-07 09:52:20 +0000

    Automatic commit by PUSH.command

--- README.md
+++ README.md
@@ -1,73 +1,8 @@
 # evolution-script-docs
-Documents for Evolution Script. https://scratch.mit.edu/projects/55653930/
+Documents for Evolution Script.
 
-Let's start with variables:
-
-Variables are made with (and changed with) "=":
-x=3
-x="derp"
-(x is the variable, "derp"/3 is the value.)
-
-Task: Create a variable called "derp" with a value of "Hi!"
-
-Alright, that's all good, but what about recalling the variable?
-
-You can use the variable name to recall the variable.
-
-x
-(returns "derp"/3)
-
-Task: Recall your derp variable.
-
-Great, but can I find if the value of x is something?
-
-Yep! You use "==" to compare.
-
-x==3
-x=="derp"
-(x is being compared to "derp"/3 and returns true/false)
-
-Huh... What about if it is NOT something?
-
-x!=3
-x!="derp"
-(checks if x is NOT something)
-
-Ok, ok! Fine...
-
-Task: Compare derp to something
-
-Hmm... What if I want to find the variable type?
-
-Well, so far, you can use .(an identifier)?
-
-x.str?
-x.bool?
-x.num?
-x.int?
-x.float?
-(they return true or false)
-
-Task: Compare derp to a boolean (.bool?)
-
-Nice, but programming languages have got to have lists... Hopefully yours doesn't start on 0...
-
-No, it doesn't! You don't even need square brackets! They are called "arrays" by the way...
-
-x="Hi!", "Hello!", 3, 4.3, 55, 90, "Thing!"
-x[4]
-(x is given items, x[4] returns 4.3 as it is the fourth item)
-
-Task: Make derp into an array of at least 3 items and return one of the items.
-
-Issues:
-
-Thanks for going through the tutorial! You may have noted some things it doesn't have.
-
-x=y is not currently possible.
-x[y] is not possible.
-Where are all the other comparing operators?
-
-Soon to come! (maybe)
+Variables - leet = 1337, derp = "Hello!", leet, derp
+Comments - @ This line gives derp a value of 1337
+Maths - HEX( 78 ), OCT( 89 ), BIN( 123 ), DO( 8 * 9 )
 
 
--- evolution-script.py
+++ evolution-script.py
@@ -7,10 +7,11 @@ class EvolutionScript(object):
     def __init__(self, f=""):
         self.setup()
         
-        if type(f) == "file":
+        if type(f) == file:
             print "You gave me a file!"
-        elif type(f) == "string":
+        elif type(f) == str:
             print "You gave me a string!"
+            self.interpretLines(f.split("\n"))
         elif type(f) == list:
             print "You gave me a list!"
             self.interpretLines(f)
@@ -18,38 +19,96 @@ class EvolutionScript(object):
             print "You gave me a %s!" % type(f)
     
     def setup(self):
-        self.variables = {}
+        self.variables = {} # Variables are stored in an dictionary.
     
     def interpretLines(self, lines):
         for l in lines:
-            self.interpretLine(l)
+            # Run line
+            out = self.interpretLine(l.strip())
+            # Output
+            if out is not None: print "=>", out
     
     def interpretLine(self, line):
-        print "INTERPRETING LINE %s" % line
-        words = self.parseLine(line)
+        words = self.parseLine(line)[0]
+        punc = self.parseLine(line)[1]
+        testing = True
+        if testing == True: # to help with visualisation
+            print words
+            print punc
+            print "Interpreting line: '%s'" % line
+        # Commands!
+        if (len(words) > 1):
+            if (punc[1] == "="):
+                #print "Found set-variable command!"
+                #print "Setting variable '%s' to '%s'." % (words[1], words[1:])
+                print len(words), words
+                value = filter(lambda x: x != "", words[1:])[0] + "'"
+                self.variables[words[0]] = value
+            elif words[0] == "@":
+                return None
+            elif words[0] == "BIN" and punc[1] == ")":
+                if words[1].isdigit():
+                    return bin(int(words[1]))
+                else:
+                    if words[1] in self.variables:
+                        return bin(int(self.variables[words[1]]))
+                    else:
+                        return "ERROR: NO VARIABLE OF THAT NAME IS DEFINED."
+            elif words[0] == "HEX" and punc[1] == ")":
+                return hex(int(words[1])) 
+            elif words[0] == "OCT" and punc[1] == ")":
+                return oct(int(words[1]))
+            elif words[0] == "DO" and punc[len(words)-2] == ")":
+                return eval(line[2:]) # Is the problem here?
+            else:
+                return "ERROR: COMMAND NOT FOUND"
+        elif (len(words) == 1):
+            if False: # Put 1 line commands after line 49
+                pass
+            else:
+                if words[0] in self.variables:
+                    return self.variables[words[0]][1:]
+                else:
+                    return "ERROR: METHOD OR VARIABLE NOT DEFINED."
+        elif len(words) == 0 or words == ['']:
+            return None
     
     def parseLine(self, line):
+        line = line.strip().replace("\n", "")
+        #print "<", line
+        
         words = []
-        punctuation = ["^", "+", "-", "*", "/", "[", "{", "]", "}", ";", "|", "&", "!", "\"", "'", "$", "=", ".", ":", " "]
+        puncOut = []
+        punctuation = ["(", ")", "^", "+", "-", "*", "/", "[", "{", "]", "}", ";", "|", "&", "!", "\"", "'", "$", "=", ".", ":", " "] # added ()
         
         # Parse line.
-        currentWord = ""
-        for i in range(0, len(line)):
-            if line[i] in punctuation:
-                print "Adding word '%s' to word list with separator '%s'" % (
-                    currentWord, line[i])
-                words.append(line[i])
-                words.append(currentWord)
-                currentWord = ""
-            else:
-                currentWord = currentWord + line[i]
-        
-        words.append(currentWord) # Add the last word to the list.
-        print "Finished parsing line! Output:", words
-        return words
+        if (line is not None and line is not "\n"):
+            currentWord = ""
+            inString = False
+            for i in range(0, len(line)):
+                if (line[i] == "\"" or line[i] == "'"): # Detect string
+                    inString = not inString
+                
+                if (line[i] in punctuation and inString is False):
+                    puncOut.append(line[i])
+                    words.append(currentWord)
+                    currentWord = ""
+                else:
+                    currentWord = currentWord + line[i]
+    
+            
+            words.append(currentWord) # Add the last word to the list.
+        return (words, puncOut)
 
 if (__name__ == '__main__'):
-    e = EvolutionScript([
-        "X = HELLO WORLD",
-        "X"
-    ])
+    new = EvolutionScript("""
+        Y = 'THIS USES A MULTI-LINE STRING!'
+        Y 
+        X = 2
+        @ THIS IS A COMMENT BY MRSHERLOCKHOLMES
+        BIN(43)
+        HEX(90)
+        OCT(72) 
+        DO(2*9*5/15)
+""")
+    
